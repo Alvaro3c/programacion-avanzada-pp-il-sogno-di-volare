e3b2fdda91434120ed1a1e43869ea67386bdd321
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-06-19 14:01:40 +0200

    add/excercise finished

--- scripts.js
+++ scripts.js
@@ -32,35 +32,67 @@ const militaryHelicopter = [
 
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages;
+        this.militaryImages = militaryImages;
     }
-    
+
     getRandomCivil() {
+        return this.civilImages[Math.floor(Math.random() * this.civilImages.length)]
     }
-    
+
     getRandomMilitary() {
+        return this.militaryImages[Math.floor(Math.random() * this.militaryImages.length)]
     }
-    
+
     getAll() {
+        return [...this.civilImages, ...this.militaryImages]
     }
 }
 
 class Painter {
     constructor() {
+        this.gallery = document.createElement('section');
     }
 
     createGallery() {
+        document.body.appendChild(this.gallery);
     }
 
     createImageTag(imageUrl) {
+        let elements = `
+        <picture>
+        <img src="${imageUrl}" />
+        </picture>`
+        return elements
     }
 
     paintSingleImage(imageUrl) {
+        this.gallery.innerHTML = this.createImageTag(imageUrl)
     }
 
     paintMultipleImages(arrayOfImages) {
+        arrayOfImages.forEach(imageURL => {
+            this.gallery.innerHTML += `
+            <picture>
+            <img src="${imageURL}" />
+            </picture>`
+        });
     }
 }
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
 const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
-const painter = new Painter();
+
+// Pruebas de Gallery con aircrafts
+console.log(aircrafts.getRandomCivil())
+console.log(aircrafts.getRandomMilitary())
+console.log(aircrafts.getAll())
+
+// Pruebas de Gallery con helicopters
+console.log(helicopters.getRandomCivil())
+console.log(helicopters.getRandomMilitary())
+console.log(helicopters.getAll())
+
+const painter = new Painter()
+painter.createGallery()
+painter.createImageTag('assets/helicoptero-militar-3.jpg')
